c6f52701470c6b97ebb3ebb94cf16baa3af89c6b
himchan <himchan9494@gmail.com>
2021-11-23 15:13:15 +0900

    FizzBuzz version 2
    Using Array & forEach print out FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,42 @@
-let i = 1;
+// project 1
 
-while (i < 101) {
-  if (i % 3 === 0) {
-    if (i % 15 === 0) {
-      console.log("fizzbuzz");
-    } else {
-      console.log("fizz");
-    }
-  } else if (i % 5 === 0) {
-    if (i % 15 === 0) {
-      console.log("fizzbuzz");
-    } else {
-      console.log("buzz");
-    }
-  } else {
-    console.log(i);
-  }
-  i++;
-}
+// let i = 1;
+
+// while (i < 101) {
+// if (i % 3 === 0) {
+//   if (i % 15 === 0) {
+//     console.log("fizzbuzz");
+//   } else {
+//     console.log("fizz");
+//   }
+// } else if (i % 5 === 0) {
+//   if (i % 15 === 0) {
+//     console.log("fizzbuzz");
+//   } else {
+//     console.log("buzz");
+//   }
+// } else {
+//   console.log(i);
+// }
+//   i++;
+// }
+
+// project 2
+
+// Array.from({ length: 100 }, (_, i) => i + 1).forEach((i) => {
+//   if (i % 3 === 0) {
+//     if (i % 15 === 0) {
+//       console.log("fizzbuzz");
+//     } else {
+//       console.log("fizz");
+//     }
+//   } else if (i % 5 === 0) {
+//     if (i % 15 === 0) {
+//       console.log("fizzbuzz");
+//     } else {
+//       console.log("buzz");
+//     }
+//   } else {
+//     console.log(i);
+//   }
+// });
